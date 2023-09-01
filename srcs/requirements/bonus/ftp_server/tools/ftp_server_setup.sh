708c00092b815b34aef13e89446e1efcea50e60e
MouadxBth <mouad.bouthaich@outlook.com>
2023-09-01 14:22:28 +0100

    ~Gradually adding the bonus containers
    Starting with the ftp server
    
    Forgot that volumes mount after the container starts

---
+++ srcs/requirements/bonus/ftp_server/tools/ftp_server_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+chmod -R 750 /var/www/wordpress 
+
+chown -R ftpmbouthai:ftpmbouthai  /var/www/wordpress 
+
+echo "ftpmbouthai" >> /etc/vsftpd.userlist 
+
+echo "ftpmbouthai:123456" | /usr/sbin/chpasswd 
+
+echo "write_enable=YES" >> /etc/vsftpd.conf 
+
+echo "chroot_local_user=YES" >> /etc/vsftpd.conf 
+
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf 
+
+echo "userlist_enable=YES" >> /etc/vsftpd.conf 
+
+echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf 
+
+echo "userlist_deny=NO" >> /etc/vsftpd.conf
+
+exec "/usr/sbin/vsftpd" "/etc/vsftpd.conf"
