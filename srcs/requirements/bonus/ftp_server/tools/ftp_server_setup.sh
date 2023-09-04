4122abad20da2ba463586eb88957f4aa8702493a
MouadxBth <mouad.bouthaich@outlook.com>
2023-09-04 10:28:38 +0100

    ~Gradually adding the bonus containers
    Starting with the ftp server
    
    Attempting to fix the login with system accounts

--- srcs/requirements/bonus/ftp_server/tools/ftp_server_setup.sh
+++ srcs/requirements/bonus/ftp_server/tools/ftp_server_setup.sh
@@ -2,6 +2,8 @@
 
 echo /usr/sbin/nologin >> /etc/shells
 
+chmod -R 755 /var/www && chmod -R 755 /var/www/wordpress 
+
 adduser --system \
     --home /var/www/wordpress \
     --shell /usr/sbin/nologin \
@@ -11,8 +13,6 @@ adduser --system \
 echo "${FTP_USER}:${FTP_PASSWORD}" | /usr/sbin/chpasswd 
 
 echo "account sufficient pam_permit.so" >> /etc/pam.d/vsftpd
-
-chmod -R 750 /var/www/wordpress 
 
 chown -R ${FTP_USER}:${FTP_USER}  /var/www/wordpress 
 
@@ -37,6 +37,12 @@ echo "pasv_enable=YES" >> /etc/vsftpd.conf
 echo "pasv_min_port=40100" >> /etc/vsftpd.conf
 
 echo "pasv_max_port=40200" >> /etc/vsftpd.conf
+
+echo -e "debug_enable=YES\n
+dual_log_enable=YES\n
+syslog_enable=YES\n
+log_ftp_protocol=YES\n
+xferlog_enable=YES\n" >> /etc/vsftpd.conf
 
 service vsftpd start
 
