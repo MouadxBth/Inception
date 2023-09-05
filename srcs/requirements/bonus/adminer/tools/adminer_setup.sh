a4b632dc74853fa7771a061ab946449ae322d0c5
MouadxBth <mouad.bouthaich@outlook.com>
2023-09-05 11:01:33 +0100

    ~Gradually adding the bonus containers
    Next up, Adminer
    
    Attempting initial configuration

--- srcs/requirements/bonus/adminer/tools/adminer_setup.sh
+++ srcs/requirements/bonus/adminer/tools/adminer_setup.sh
@@ -8,6 +8,6 @@ mkdir -p /var/www/html
 
 curl https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -o /var/www/html/index.php
 
-COPY /conf/www.conf /etc/php/${PHP_VERSION}/fpm/php-fpm.conf
+mv /conf /etc/php/${PHP_VERSION}/fpm/php-fpm.conf
 
 exec php-fpm${PHP_VERSION} -F
