dd0a11b13323f80a4c7e4d08a0558b8c09409878
Mouad Bouthaich <mouad.bouthaich@outlook.com>
2023-10-09 14:41:50 +0100

    ~Fixed mariadb

--- srcs/requirements/mariadb/tools/wordpress_db_setup.sh
+++ srcs/requirements/mariadb/tools/wordpress_db_setup.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 
-/etc/init.d/mariadb start
+sed --in-place "s|^socket|#socket|g" /etc/mysql/my.cnf
+sed --in-place "s|^# port|port|g" /etc/mysql/my.cnf
 
-mysqladmin -u root password "$MARIADB_ROOT_PASSWORD"
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "user = mysql" >> /etc/mysql/my.cnf
+echo "port = 3306" >> /etc/mysql/my.cnf
+echo "datadir = /var/lib/mysql" >> /etc/mysql/my.cnf
+echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
+echo "wait_timeout = 3600" >> /etc/mysql/my.cnf
+
+sed --in-place "s|^bind-address            = 127.0.0.1|bind-address            = 0.0.0.0|" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mariadb start
+
+while ! mysqladmin ping -h localhost --silent 2>/dev/null; do
+ 	sleep 1
+done
+
+expect -c "
+
+set timeout 1
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none): \"
+send \"$MARIADB_ROOT_PASSWORD\r\"
+expect \"Switch to unix_socket authentication \[Y/n\] \"
+send \"y\r\"
+expect \"Change the root password? \[Y/n\] \"
+send \"n\r\"
+expect \"Remove anonymous users? \[Y/n\] \"
+send \"y\r\"
+expect \"Disallow root login remotely? \[Y/n\] \"
+send \"y\r\"
+expect \"Remove test database and access to it? \[Y/n\] \"
+send \"y\r\"
+expect \"Reload privilege tables now? \[Y/n\] \"
+send \"y\r\"
+expect eof
+"
+
+# mysqladmin -u root password "$MARIADB_ROOT_PASSWORD"
 
 cat << _EOF_ > /tmp/db.sql
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
 CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
 FLUSH PRIVILEGES;
 _EOF_
 
-echo "FIRST:"
 mariadb -u root -p"$MARIADB_ROOT_PASSWORD" < /tmp/db.sql
 
-sleep 3
-
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-echo "THIRD:"
 mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
 
 exec mysqld
